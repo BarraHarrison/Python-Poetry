81f7bdac626fb25ea735bf5ecb1bd1af9337dbd8
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2024-12-10 16:32:35 +0900

    text to numerical format

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ from tensorflow.keras.optimizers import RMSprop
 filepath = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
 
 text = open(filepath, 'rb').read(encoding='utf-8').lower()
+
+# Converting the text into a numerical format
+text = text[300000:800000]
+characters = sorted(set(text))
+
+char_to_index = dict((c, i) for i, c in enumerate(characters))
+index_to_char = dict((i, c) for i, c in enumerate(characters))
+
